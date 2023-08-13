d606661a7ff7bfa88939db4073fdc1703a331c8e
jexi-The Linux <vnjeru85@gmail.com>
2023-08-13 22:03:30 +0300

    sum digits using tail recursive

--- lib/recursion/sum_digits.ex
+++ lib/recursion/sum_digits.ex
@@ -2,6 +2,15 @@ defmodule ElixirMixLearn.Recursion.SumDigits do
   def upto(0), do: 0
 
   def upto(num) do
-    num + upto(num - 1)
+    upto_tail_rec(num)
+  end
+
+  # Public function - for documentation of code
+  def upto_tail_rec(num, acc \\ 0)
+  # Base case
+  def upto_tail_rec(0, acc), do: acc
+
+  def upto_tail_rec(num, acc) do
+    upto_tail_rec(num - 1, acc + num)
   end
 end
