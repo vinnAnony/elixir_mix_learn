6d63c07af8cc61efe3c43ede8e7698783a6c4f61
jexi-The Linux <vnjeru85@gmail.com>
2023-08-14 20:41:57 +0300

    add some documentation

--- lib/lists/lists.ex
+++ lib/lists/lists.ex
@@ -1,9 +1,21 @@
 defmodule ElixirMixLearn.Lists do
+  @moduledoc """
+  Function Summary:
+
+  1. sum of numbers in a list
+  """
+
+  @doc """
+  Returns sum of numbers in a list
+  """
+  @spec sum(list(number())) :: number()
   def sum(nums), do: sum_tail_rec(nums)
 
+  @spec simple_sum(list(number())) :: number()
   def simple_sum([]), do: 0
   def simple_sum([h | t]), do: h + simple_sum(t)
 
+  @spec sum_tail_rec(list(number()), integer()) :: number()
   def sum_tail_rec(nums, acc \\ 0)
   def sum_tail_rec([], acc), do: acc
 
