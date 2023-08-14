8f4890e49b33b464da5de7d65697fc7478f2d5b5
jexi-The Linux <vnjeru85@gmail.com>
2023-08-15 00:39:53 +0300

    add more functionalities

--- lib/structs/seven_wonders.ex
+++ lib/structs/seven_wonders.ex
@@ -26,4 +26,26 @@ defmodule ElixirMixLearn.Structs.SevenWonders do
   def filter_by_country(wonders, country) do
     wonders |> Enum.filter(fn %{country: country_name} -> country_name == country end)
   end
+
+  @spec sort_by_country_length([t()]) :: [t()]
+  def sort_by_country_length(wonders) do
+    wonders |> Enum.sort(fn x, y -> String.length(x.country) < String.length(y.country) end)
+  end
+
+  @spec name_country_list([t()]) :: [t()]
+  def name_country_list(wonders) do
+    wonders
+    |> Enum.reduce([], fn wonder, acc -> [[wonder.name, wonder.country] | acc] end)
+  end
+
+  @spec country_name_keyword([t()]) :: [Atom]
+  def country_name_keyword(wonders) do
+    wonders
+    |> Enum.reduce([], fn wonder, acc -> [{String.to_atom(wonder.name), wonder.country} | acc] end)
+  end
+
+  @spec all_names([t()]) :: [String]
+  def all_names(wonders) do
+    for %{name: name} <- wonders, do: name
+  end
 end
