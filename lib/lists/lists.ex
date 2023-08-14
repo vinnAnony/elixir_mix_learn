fdc7b59ac14815c31680af502d08d0a25928ded2
jexi-The Linux <vnjeru85@gmail.com>
2023-08-14 20:37:31 +0300

    sum of list using tail recursion

--- lib/lists/lists.ex
+++ lib/lists/lists.ex
@@ -1,4 +1,13 @@
 defmodule ElixirMixLearn.Lists do
+  def sum(nums), do: sum_tail_rec(nums)
+
   def simple_sum([]), do: 0
   def simple_sum([h | t]), do: h + simple_sum(t)
+
+  def sum_tail_rec(nums, acc \\ 0)
+  def sum_tail_rec([], acc), do: acc
+
+  def sum_tail_rec([h | t], acc) do
+    sum_tail_rec(t, acc + h)
+  end
 end
