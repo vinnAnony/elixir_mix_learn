4f36d768efec18709820d5709cc050e64cf2784e
jexi-The Linux <vnjeru85@gmail.com>
2023-08-14 18:49:18 +0300

    factorial using tail recursive

--- lib/recursion/factorial.ex
+++ lib/recursion/factorial.ex
@@ -2,6 +2,13 @@ defmodule ElixirMixLearn.Recursion.Factorial do
   def of(1), do: 1
 
   def of(num) do
-    num * of(num - 1)
+    of_tail_rec(num)
+  end
+
+  def of_tail_rec(num, acc \\ 1)
+  def of_tail_rec(1, acc), do: acc
+
+  def of_tail_rec(num, acc) do
+    of_tail_rec(num - 1, acc * num)
   end
 end
